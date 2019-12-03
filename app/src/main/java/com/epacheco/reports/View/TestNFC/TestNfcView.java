d49f1ca07e470ebc7fde03e41a5adf67f4d327e4
Dalvik31 <10030167@itesa.edu.mx>
2019-12-03 00:09:58 -0600

    otro cambio

--- app/src/main/java/com/epacheco/reports/View/TestNFC/TestNfcView.java
+++ app/src/main/java/com/epacheco/reports/View/TestNFC/TestNfcView.java
@@ -49,7 +49,8 @@ public class TestNfcView extends AppCompatActivity implements NfcAdapter.OnNdefP
     setContentView(R.layout.activity_test_nfc_view);
     tex = findViewById(R.id.textView);
     setUpAndroidBeam();
-
+    Log.e("agregamos","linea");
+    Log.e("agregamos","linea");
   }
 
 
