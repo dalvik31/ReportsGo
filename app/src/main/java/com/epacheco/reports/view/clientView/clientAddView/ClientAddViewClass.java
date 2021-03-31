73962a36ef51419918fb3772b526312970553051
rancell <isra.juarez1821@gmail.com>
2021-03-31 15:10:10 -0600

    Se creo boton para crear contacto desde los contactos

--- app/src/main/java/com/epacheco/reports/view/clientView/clientAddView/ClientAddViewClass.java
+++ app/src/main/java/com/epacheco/reports/view/clientView/clientAddView/ClientAddViewClass.java
@@ -1,21 +1,42 @@
 package com.epacheco.reports.view.clientView.clientAddView;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentActivity;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.epacheco.reports.Model.ClientModel.ClientAddModel.ClientAddModelClass;
 import com.epacheco.reports.Pojo.Client.Client;
 import com.epacheco.reports.R;
 import com.epacheco.reports.Tools.ReportsDialogGlobal;
 import com.epacheco.reports.Tools.ReportsProgressDialog;
 import com.epacheco.reports.databinding.ActivityClientAddViewClassBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ClientAddViewClass extends AppCompatActivity implements ClientAddViewInterface{
 
   public final static String CLIENT_ID = "clientId";
+  static final int PICK_CONTACT_REQUEST=1;
+  private static final int READ_CONTACTS_PERMISSION = 2;
+  public static Uri contactUri;
 
   private ClientAddModelClass clientAddModelClass;
   private ActivityClientAddViewClassBinding binding;
@@ -24,12 +45,16 @@ public class ClientAddViewClass extends AppCompatActivity implements ClientAddVi
   private double limitSendClient;
   private String clientId;
   private ReportsProgressDialog progressbar;
+  private EditText txt_client_name;
+  private EditText txt_client_phone;
+  private RelativeLayout Rltv_Contact;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = DataBindingUtil.setContentView(this,R.layout.activity_client_add_view_class);
     inicializateElements();
     validateModifyClient();
+    Rltv_Contact = findViewById(R.id.Rltv_Contact);
   }
 
 
@@ -208,6 +233,125 @@ public class ClientAddViewClass extends AppCompatActivity implements ClientAddVi
   private void hideProgress(){
     progressbar.hideProgress();
     binding.progressUploadProduct.setVisibility(View.GONE);
+  }
+
+
+  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    super.onActivityResult(requestCode, resultCode, intent);
+    if (requestCode == PICK_CONTACT_REQUEST) {
+      if (resultCode == RESULT_OK) {
+
+        contactUri = intent.getData();
+
+        renderContact(contactUri);
+      }
+    }
+  }
+
+  private void renderContact(Uri uri) {
+
+   txt_client_name = findViewById(R.id.txt_client_name);
+   txt_client_phone = findViewById(R.id.txt_client_phone);
+
+
+    txt_client_name.setText(getName(uri));
+    txt_client_phone.setText(getPhone(uri));
+
+  }
+
+  private String getName(Uri uri) {
+
+    String name = null;
+
+    ContentResolver contentResolver = getContentResolver();
+
+    Cursor c = contentResolver.query(
+            uri, new String[]{ContactsContract.Contacts.DISPLAY_NAME}, null, null, null);
+
+    if(c.moveToFirst()){
+      name = c.getString(0);
+    }
+
+    c.close();
+
+    return name;
+  }
+
+  private String getPhone(Uri uri) {
+    String id = null;
+    String phone = null;
+
+    Cursor contactCursor = getContentResolver().query(
+            uri, new String[]{ContactsContract.Contacts._ID},
+            null, null, null);
+
+
+    if (contactCursor.moveToFirst()) {
+      id = contactCursor.getString(0);
+    }
+    contactCursor.close();
+
+    String selectionArgs =
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                    ContactsContract.CommonDataKinds.Phone.TYPE+"= " +
+                    ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+
+    Cursor phoneCursor = getContentResolver().query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            new String[] { ContactsContract.CommonDataKinds.Phone.NUMBER },
+            selectionArgs
+            ,
+            new String[] { id },
+            null
+    );
+    if (phoneCursor.moveToFirst()) {
+      phone = phoneCursor.getString(0);
+    }
+    phoneCursor.close();
+
+    return phone;
+  }
+
+  public void Search_contact(View v){
+
+    requestCameraPermission();
+
+  }
+
+  private void requestCameraPermission() {
+
+    //shouldShowRequestPermissionRationale nos dice si el usuairo rechazo alguna vez los permisos
+    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+
+      //El usuario vuelve a dar clic en el boton se muestra un mensaje explicando al usuario por que es necesario conceder los permisos.
+      Snackbar.make(Rltv_Contact, "Requiere aceptar permiso para acceder a contactos ", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          ActivityCompat.requestPermissions(ClientAddViewClass.this, new String[]{Manifest.permission.READ_CONTACTS}, READ_CONTACTS_PERMISSION );
+        }
+      }).show();
+    }else{
+      //El usuario entra por primera vez y da clic en el boton se muestra la ventana para aceptar  los persmisos
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, READ_CONTACTS_PERMISSION );
+    }
+
+  }
+
+  private void Obtener_contacto(){
+
+    Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+    startActivityForResult(i, PICK_CONTACT_REQUEST);
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+      Obtener_contacto();
+    }else{
+      Toast.makeText(this,"que mal no has aceptado",Toast.LENGTH_SHORT).show();
+    }
   }
 
 }
