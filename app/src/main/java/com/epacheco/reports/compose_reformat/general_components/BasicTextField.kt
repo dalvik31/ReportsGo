0c466a5a22496b6ad1aa21466f687042228b4c6e
RANCELL ISRAEL JUAREZ BAÑOS <rancell.juarez@creacionestecnologicas.mx>
2025-06-27 13:47:45 -0600

    [UPDATE] - basictextfield configuration

--- app/src/main/java/com/epacheco/reports/compose_reformat/general_components/BasicTextField.kt
+++ app/src/main/java/com/epacheco/reports/compose_reformat/general_components/BasicTextField.kt
@@ -1,24 +1,28 @@
 package com.epacheco.reports.compose_reformat.general_components
 
+import android.inputmethodservice.Keyboard
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun ShowBasicTextField(textLabel : String = "Ingresa texto") {
+fun ShowBasicTextField(textLabel : String = "Ingresa texto", keyboard: KeyboardType = KeyboardType.Text, textValue : (String) -> Unit = {}) {
     val value = remember { mutableStateOf("") }
 
     OutlinedTextField(
         value = value.value,
         onValueChange = { newValue ->
             value.value = newValue
+            textValue.invoke(value.value)
         },
         label = {
             Text(
@@ -28,6 +32,7 @@ fun ShowBasicTextField(textLabel : String = "Ingresa texto") {
         },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboard)
     )
 }
